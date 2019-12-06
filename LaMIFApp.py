5ef4b3f3018fa9483a30bfdaa508f3e11e2f6525
Professeur Gibaud <mail@professeurgibaud.ovh>
2019-12-06 16:21:18 +0100

    Ajout des Memes et des corrections (problème sur les fractions et Exo trop durs)

--- LaMIFApp.py
+++ LaMIFApp.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
 from kivy.uix.label import Label
@@ -38,11 +39,56 @@ sm = ScreenManager()
 
 class ScreenAcceuil(Screen):
     def build(self):
+        self.name = "Acceuil"
         Layout = BoxLayout(orientation = "vertical")
-        Picture = Image(source = "/Image/Acceuil.jpeg")
-
-
+        Picture = Image(source = "Image/Acceuil.png")
+        Layout.add_widget(Picture)
+        BoutonSuite = Button(text = "Le Meme !!!")
+        BoutonSuite.size_hint_y = 0.3
+        BoutonSuite.bind(on_press = self.VersSuite)
+        Layout.add_widget(BoutonSuite)
         self.add_widget(Layout)
+        sm.add_widget(self)
+    def VersSuite(self,instance):
+        sm.current = "Meme"
+
+class ScreenMeme(Screen):
+    def build(self):
+        self.name = "Meme"
+        self.pret = 0
+        Layout = GridLayout(cols = 1)
+        sourceImage = rd.choice(os.listdir("Meme"))
+        self.Imagee = Image(source = "Meme/"+ sourceImage)
+        Layout.add_widget(self.Imagee)
+        BoutonLayout = BoxLayout()
+        self.BoutonLatex = Button(text = "Lancer Préparation")
+        self.BoutonLatex.bind(on_press = self.PreparerLatex)
+        self.BoutonGo = Button(text = "Pas Pret")
+        BoutonLayout.size_hint_y = 0.1
+        BoutonLayout.add_widget(self.BoutonLatex)
+        Layout.add_widget(BoutonLayout)
+        self.add_widget(Layout)
+        sm.add_widget(self)
+    def PreparerLatex(self,instance):
+        if self.pret == 1:
+            sm.current = "Enoncé1"
+        if self.pret == 0:
+            self.BoutonLatex.text = "En Cours"
+            FaireExercice()
+            ScreenEnonce = []
+            ScreenCorrection = []
+            for i in range(5):
+                Screen = ScreenLatex()
+                nomScreen = "Enoncé" + str(i)
+                Screen.build("Image/" + ListeNomFigureQuestion[i] + ".png",ListeConsigne[i],nomScreen)
+                ScreenEnonce.append(Screen)
+            for i in range(5):
+                Screen = ScreenLatex()
+                nomScreen = "Correction"+str(i)
+                Screen.build("Image/"+ListeNomFigureReponse[i]+".png",ListeConsigneCorrection[i],nomScreen)
+                ScreenCorrection.append(Screen)
+            self.pret = 1
+            self.BoutonLatex.text = "C'est prêt ! "
 
 class ScreenLatex(Screen):
     def build(self,StrImage,StrTexte,name):
@@ -53,7 +99,6 @@ class ScreenLatex(Screen):
         Layout.build(self.StrImage,self.StrTexte,self.name)
         self.add_widget(Layout)
         sm.add_widget(self)
-
 
 class LayoutLatex(BoxLayout):
     def build(self,StrImage,StrTexte,name):
@@ -72,10 +117,13 @@ class LayoutLatex(BoxLayout):
         self.add_widget(enter)
     def Suivant(self,instance):
         if self.name != "Enoncé4":
-            dest = self.name[:-1]
-            i = int(self.name[len(self.name)-1])
-            dest = dest + str(i+1)
-            sm.current = dest
+            if self.name != "Correction4":
+                dest = self.name[:-1]
+                i = int(self.name[len(self.name)-1])
+                dest = dest + str(i+1)
+                sm.current = dest
+        if self.name == "Enoncé4":
+            sm.current = "Correction0"
 
 
 
@@ -85,13 +133,11 @@ class LayoutLatex(BoxLayout):
 class MyApp(App):
     def build(self):
         Window.clearcolor = (1, 1, 1, 1)
-        ScreenEnonce = []
-        for i in range(5):
-            Screen = ScreenLatex()
-            nomScreen = "Enoncé" + str(i)
-            Screen.build(ListeNomFigureQuestion[i] + ".png",ListeConsigne[i],nomScreen)
-            ScreenEnonce.append(Screen)
-        sm.current = ScreenEnonce[0].name
+        ScreenAcceuill = ScreenAcceuil()
+        ScreenAcceuill.build()
+        ScreenMemee = ScreenMeme()
+        ScreenMemee.build()
+        sm.current = "Acceuil"
         return sm
 
 ListeNomFigureReponse = ["R1","R2","R3","R4","R5"]
@@ -102,22 +148,24 @@ ListeConsigneCorrection = []
 Type = ["D\\'eveloppement","In\\'equation","Equation","Fraction","Tableaux de Variation"]
 
 
-for i in range(5):
-    Exo = Exercice()
-    u = rd.randint(0,4)
-    Exo.type = Type[u]
-    Exo.classe = "Terminale"
-    Exo.FaireEnonce()
-    Exo.FaireCorrection()
-    Exo.EcrireExo()
-    Exo.EcrireCorrection()
-    ListeConsigne.append(Exo.consigne)
-    ListeConsigneCorrection.append(Exo.consignecorrection)
-    Exo.VersLatex()
-    FormulaEnonce = Exo.enonce
-    FormulaCorrection = Exo.correction
-    formula_as_file(FormulaEnonce,ListeNomFigureQuestion[i] + ".png")
-    formula_as_file(FormulaCorrection,ListeNomFigureReponse[i]+ ".png")
+
+def FaireExercice():
+    for i in range(5):
+        Exo = Exercice()
+        u = rd.randint(0,4)
+        Exo.type = Type[u]
+        Exo.classe = "Terminale"
+        Exo.FaireEnonce()
+        Exo.FaireCorrection()
+        Exo.EcrireExo()
+        Exo.EcrireCorrection()
+        ListeConsigne.append(Exo.consigne)
+        ListeConsigneCorrection.append(Exo.consignecorrection)
+        Exo.VersLatex()
+        FormulaEnonce = Exo.enonce
+        FormulaCorrection = Exo.correction
+        formula_as_file(FormulaEnonce,"Image/"+ ListeNomFigureQuestion[i] + ".png")
+        formula_as_file(FormulaCorrection,"Image/" + ListeNomFigureReponse[i]+ ".png")
 
 
 print(ListeConsigne)
